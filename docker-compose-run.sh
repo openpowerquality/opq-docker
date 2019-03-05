df5d0d290e960a6d017dcdbf1496c5860f9ecb46
David Aghalarpour <aghalarp@hawaii.edu>
2019-03-05 10:43:48 -0500

    Fix expected View config directory

--- docker-compose-run.sh
+++ docker-compose-run.sh
@@ -6,7 +6,7 @@
 # See: https://docs.docker.com/compose/compose-file/#variable-substitution
 
 # View Config Environment Var
-export METEOR_SETTINGS=$(cat ./config/view.config.json)
+export METEOR_SETTINGS=$(cat ./config/view/view.config.json)
 
 # Startup Docker-Compose. Note: Be sure that docker-compose.yml is same directory as this script.
 docker-compose up -d --remove-orphans
